8d07e0041c06c434b2311b8ca5b4ff21c9cc0e1f
Alef <felipexk1@live.com>
2022-08-13 04:51:54 -0300

    :construction: Remove IOS and LineBreakerRules

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  StatusBar,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {SafeAreaView, Text, View} from 'react-native';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
   return (
     <SafeAreaView>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <View>
         <Text>Hello world!</Text>
       </View>
