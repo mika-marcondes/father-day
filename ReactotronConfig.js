4774f5ceeadcc606510b3376f8faa7a3dc9c0f40
Mika <mika.marcondes2@gmail.com>
2022-08-13 03:47:19 -0300

    Configure alias

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -1,7 +1,11 @@
-import Reactotron from 'reactotron-react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import Reactotron, {networking} from 'reactotron-react-native';
 
-Reactotron.setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
-  .configure() // controls connection & communication settings
+Reactotron.configure() // controls connection & communication settings
+  .use(
+    networking({
+      ignoreContentTypes: /^(image)\/.*$/i,
+      ignoreUrls: /\/(logs|symbolicate)$/,
+    }),
+  )
   .useReactNative() // add all built-in react native plugins
   .connect(); // let's connect!
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ts', '.tsx', '.jsx', '.js', '.json'],
+        alias: {
+          '@assets/*': './src/assets/*',
+          '@components/*': './src/components/*',
+          '@hooks/*': './src/hooks/*',
+          '@screens/*': './src/screens/*',
+        },
+      },
+    ],
+  ],
 };
