25ae0f6a78ce0ef86e2b341b175e7414fbc6129d
Alef <felipexk1@live.com>
2022-08-13 09:08:34 -0300

    :heavy_plus_sign: Firebase - Firestore and Storage configuration

--- android/app/src/main/java/com/fatherday/MainApplication.java
+++ android/app/src/main/java/com/fatherday/MainApplication.java
@@ -1,6 +1,6 @@
 package com.fatherday;
 
-import android.app.Application;
+import androidx.multidex.MultiDexApplication;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
@@ -13,7 +13,7 @@ import com.fatherday.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends MultiDexApplication implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
       new ReactNativeHost(this) {
