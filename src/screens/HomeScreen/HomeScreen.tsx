b5f4d7086b9b718ad1a23e2051fd9795033624c1
Alef <felipexk1@live.com>
2022-08-14 19:00:01 -0300

    :heavy_plus_sign: Firebase - Firestore and Storage configuration (#4)

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-
-import {Container} from './style';
 import {Text} from 'react-native';
+
 import {CarouselView} from '@components';
+import {Container} from './style';
 
 export const HomeScreen: React.FC = () => {
   return (
